593f6b299b0061f6036cdcd912526d672d8fb5c6
Yuta <sanomenemo@gmail.con>
2025-03-10 20:13:13 +0700

    2.4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,8 +20,16 @@ class UserController extends Controller
         UserModel::where('username', 'customer-1')->update($data); // update data user
 
         // coba akses model UserModel
-        $user = UserModel::where('level_id', 2)->count();
-       
+        $user = UserModel::firstOrNew(
+            [
+                'username'=> 'manager33',
+                'nama'=> 'Manager Tiga Tiga',
+                'password'=> Hash::make('12345'),
+                'level_id'=>2
+            ],
+        );
+        $user->save();
+        
         return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,33 +1,25 @@
 <!DOCTYPE html>
-<html lang="id">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data User</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 200px;
-        }
-        th, td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: center;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
 </head>
 <body>
     <h1>Data User</h1>
-    <table>
+    <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <th>Jumlah Pengguna</th>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Nama</th>
+            <th>ID Level Pengguna</th>
         </tr>
+        @foreach ($data as $d)
         <tr>
-            <td>{{ $data }}</td>
+            <td>{{ $data->user_id }}</td>
+            <td>{{ $data->username }}</td>
+            <td>{{ $data->nama }}</td>
+            <td>{{ $data->level_id }}</td>
         </tr>
+        @endforeach
     </table>
 </body>
 </html>
